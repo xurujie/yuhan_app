11906e8fb70fea0ddeebd50ccb2bcb84ae348891
sometime <bing2619@163.com>
2019-09-09 22:57:45 +0800

    add img

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@ export default {
       this.$store.commit('setUser',userInfo)
 
     }
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          console.log(res.code,999)
+          wx.request({
+            url: 'https://test.com/onLogin',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   } 
    
 }
